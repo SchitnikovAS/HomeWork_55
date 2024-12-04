81d03383e34db7c6b947f8992091aed9617c7d1d
SchitnikovAS <aleksej.baxman@bk.ru>
2024-12-04 18:57:00 +0300

    Add files via upload

--- tests_12_2.py
+++ tests_12_2.py
@@ -59,19 +59,25 @@ class TournamentTest(unittest.TestCase):
         for test in cls.all_results:
             print({k: str(v) for k, v in cls.all_results[test].items()})
 
-    def test_usain_nik(self):
+    def test_first_tournament(self):
         tour = Tournament(90, self.usain, self.nik)
         results = tour.start()
         self.all_results[inspect.stack()[0][3]] = results
         self.assertTrue('Ник' == results[len(results)].name)
 
-    def test_andrey_nik(self):
+    def test_second_tournament(self):
         tour = Tournament(90, self.andrey, self.nik)
         results = tour.start()
         self.all_results[inspect.stack()[0][3]] = results
         self.assertTrue('Ник' == results[len(results)].name)
 
-    def test_nik_andrey_usain_8(self):
+    def test_third_tournament(self):
+        tour = Tournament(90, self.usain, self.andrey, self.nik)
+        results = tour.start()
+        self.all_results[inspect.stack()[0][3]] = results
+        self.assertTrue('Ник' == results[len(results)].name)
+
+    def test_third_tournament_additional(self):
         tour = Tournament(8, self.nik, self.andrey, self.usain)
         results = tour.start(0.1)
         self.all_results[inspect.stack()[0][3]] = results
